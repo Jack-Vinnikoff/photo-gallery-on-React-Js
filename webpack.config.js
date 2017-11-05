23f97830ce1f2f0ed96fedaa02b1be9504650b61
Demon <vinnicox@gmail.com>
2017-11-06 00:52:12 +0400

    start project v.1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+//var webpack = require('webpack');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path:__dirname + "/www/js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)?$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options:{presets: ['es2015','react']}
+                }]
+            }
+        ]
+    }
+}
